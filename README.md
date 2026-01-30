9db391ed6d3c33e9579444a790e96b7cebc20983
AlexMorgan3817 <Ivan.zvereff@gmail.com>
2026-01-30 12:46:54 +0700

    Adding arrows for managing regexes, remove button and case sensitivity flag for every regex

---
+++ README.md
@@ -0,0 +1,18 @@
+# Regexer
+
+![alt text](GoodBenchmark.png)
+
+
+Regexer is my rust experiment and training, solving a problem with replacing sequential regexes on the same message.
+
+An easy tool for processing (replacement) text with sequence of regexes.
+
+TODO: Extension for zed and VS Code for global replacements.
+
+I really appreciate anyone to test it and make an issue to find any other cases like `\n` and `\t`, that isn't working
+
+![alt text](.github/case_sens.png)
+
+![alt text](.github/case_insens.png)
+
+![alt text](.github/release_build.png)
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ use eframe::egui;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct RegexRule {
 	pattern: String,
 	replacement: String,
+	case_sensitive: bool,
+}
+impl RegexRule {
+    fn default() -> Self {
+        Self {
+            pattern: String::new(),
+            replacement: String::new(),
+            case_sensitive: true,
+        }
+    }
 }
 
 #[derive(Default, Serialize, Deserialize)]
@@ -16,8 +26,120 @@ struct RegexReplacerApp {
 	errors: Vec<String>,
 	width:String
 }
+
+impl eframe::App for RegexReplacerApp {
+	fn save(&mut self, storage: &mut dyn eframe::Storage) {
+		eframe::set_value(storage, eframe::APP_KEY, self);
+	}
+	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+		ctx.set_visuals(egui::Visuals::dark());
+		egui::CentralPanel::default().show(ctx, |ui| {
+			egui::ScrollArea::both().auto_shrink([false, false]).show(ui, |ui|{
+				let available = ui.available_width();
+				self.width = format!("{}", available);
+				// Width and errors debug
+				if cfg!(debug_assertions){
+					ui.text_edit_singleline(&mut self.width);
+					ui.vertical(|ui|{
+						ui.label("Errors");
+						ui.text_edit_multiline(&mut self.errors.join("\n"));
+					});
+				}
+				// Input / Output
+				if available > 450.0
+					{ui.horizontal(|ui| {self.input_output(ui)});}
+				else
+					{ui.vertical(|ui| {self.input_output(ui)});}
+				// Regexes table
+				ui.vertical(|ui|{
+					self.regexes_table(ui, &available);
+				});
+			});
+		});
+	}
+}
+
 impl RegexReplacerApp {
-	fn add_button(&mut self, ui: &mut egui::Ui){
+	fn button_apply_regexes(&mut self){
+		let mut text = self.input_text.clone();
+		let mut errored = false;
+		for idx in (0..self.rules.len()).rev(){
+			let rule = &self.rules[idx];
+			let p = &rule.pattern;
+			let px = if rule.case_sensitive { p.clone() } else { format!("(?i){}", p)};
+			match Regex::new(&px){
+				Ok(pattern) => {
+					text = pattern
+						.replace_all(&text, rule.replacement.replace("\\n", "\n"))
+						.to_string();
+				}
+				Err(e) => {
+					self.errors.push(format!("Error: {e}"));
+					errored = true;
+					break;
+				}
+			}
+		}
+		if !errored{
+			self.output_text = text;
+		}
+	}
+
+	fn regexes_table(&mut self, ui: &mut egui::Ui, available: &f32){
+		ui.heading("Regexes");
+		if ui.button("Apply").clicked(){
+			self.button_apply_regexes();
+		}
+		self.button_add_regex(ui);
+		// Table itself
+		for idx in (0..(&self.rules).len()).rev(){
+			ui.horizontal(|ui|{
+				if *available > 450.0 {
+					ui.horizontal(|ui|{
+						self.regex_stack(ui, idx);
+					});
+				} else {
+					ui.vertical(|ui|{
+						self.regex_stack(ui, idx);
+						ui.separator();
+					});
+				}
+			});
+		}
+		self.button_add_regex(ui);
+	}
+
+	fn regex_stack(&mut self, ui: &mut egui::Ui, idx:usize){
+		let l =  self.rules.len();
+		let r = self.rules.get_mut(idx);
+		match r {
+			Some(rule) => {
+				ui.text_edit_singleline(&mut rule.pattern);
+				ui.text_edit_singleline(&mut rule.replacement);
+				if ui.button("-").clicked(){
+					self.rules.remove(idx);
+					return;
+				}
+				ui.checkbox(&mut rule.case_sensitive, "Case Sensitive");
+				ui.horizontal(|ui|{
+					if ui.button("/\\").clicked(){
+						if idx < l - 1{
+							self.rules.swap(idx, idx + 1);
+						}
+					}
+					if ui.button("\\/").clicked(){
+						if idx > 0{
+							self.rules.swap(idx, idx - 1);
+						}
+					}
+				});
+			},
+			None => {
+				ui.label(format!("Error: No rule found at index {}", idx));
+			}
+		}
+	}
+	fn button_add_regex(&mut self, ui: &mut egui::Ui){
 		if ui.button("+").clicked(){
 			self.rules.push(RegexRule::default());
 		}
@@ -31,66 +153,6 @@ impl RegexReplacerApp {
 			ui.label("Out");
 			ui.text_edit_multiline(&mut self.output_text);
 		});
-	}
-}
-fn regex_stack(ui: &mut egui::Ui, rule:&mut RegexRule){
-	ui.text_edit_singleline(&mut rule.pattern);
-	ui.text_edit_singleline(&mut rule.replacement);
-}
-impl eframe::App for RegexReplacerApp {
-	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-		ctx.set_visuals(egui::Visuals::dark());
-		egui::CentralPanel::default().show(ctx, |ui| {
-			egui::ScrollArea::both().auto_shrink([false, false]).show(ui, |ui|{
-				let available = ui.available_width();
-				self.width = format!("{}", available);
-				ui.text_edit_singleline(&mut self.width);
-				ui.vertical(|ui|{
-					ui.label("Errors");
-					ui.text_edit_multiline(&mut self.errors.join("\n"));
-				});
-				if available > 450.0 {
-					ui.horizontal(|ui| {self.input_output(ui);});
-				} else {
-					ui.vertical(|ui| {self.input_output(ui);});
-				}
-				ui.vertical(|ui|{
-					ui.heading("Regexes");
-					if ui.button("Apply").clicked(){
-						let mut text = self.input_text.clone();
-						let mut errored = false;
-						for rule in &self.rules{
-							match Regex::new(&rule.pattern){
-								Ok(re) => {
-									text = re
-										.replace_all(&text, rule.replacement.replace("\\n", "\n"))
-										.to_string();
-								}
-								Err(e) => {
-									self.errors.push(format!("Error: {e}"));
-									errored = true;
-									break;
-								}
-							}
-						}
-						if !errored{self.output_text = text;}
-					}
-					self.add_button(ui);
-					for rule in &mut self.rules{
-						if available > 450.0 {
-							ui.horizontal(|ui|{regex_stack(ui, rule);});
-						} else {
-							ui.vertical(|ui|{regex_stack(ui, rule);});
-							ui.separator();
-						}
-					}
-					self.add_button(ui);
-				});
-			});
-		});
-	}
-	fn save(&mut self, storage: &mut dyn eframe::Storage) {
-		eframe::set_value(storage, eframe::APP_KEY, self);
 	}
 }
 
